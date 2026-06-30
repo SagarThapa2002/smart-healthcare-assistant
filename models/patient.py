7f63516024a0bc32059cb857ec518000d97419c0
Sagar Thapa <2059sagarthapa@gmail.com>
2026-06-30 18:20:03 +0100

    Initialise patient management

--- models/patient.py
+++ models/patient.py
@@ -1,14 +1,26 @@
+from datetime import date
+
 class Patient:
 
-    def __init__(self, patient_id, name, age, gender):
+    def __init__(
+            self,
+            patient_id: str,
+            first_name: str,
+            last_name: str,
+            date_of_birth: date,
+            gender: str,
+            phone: str,
+            email: str ="",
+            address: str = "",
+            registration_date: date | None = None,
+    ) -> None:
+        
         self.patient_id = patient_id
-        self.name = name
-        self.age = age
+        self.first_name = first_name.strip()
+        self.last_name = last_name.strip()
+        self.date_of_birth = date_of_birth
         self.gender = gender
-
-    def display_info(self):
-        print("\nPatient Information")
-        print(f"ID: {self.patient_id}")
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Gender: {self.gender}")
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.registration_date = registration_date or date.today()
